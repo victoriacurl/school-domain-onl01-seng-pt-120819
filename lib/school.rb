8ed0e935f8145175b8dfce73976837c2f206902d
Victoria Curl <victoriacurl9@gmail.com>
2020-01-17 20:34:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,7 @@
 school = School.new("Bayside High School")
 
-school.roster
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
 school.add_student("Zach Morris", 9)
 school.roster
