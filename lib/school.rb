36ed943419b6da05d01e88f739adfdddae3d1869
Victoria Curl <victoriacurl9@gmail.com>
2020-01-17 21:09:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+school = School.new("Bayside High School")
+
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.add_student("Zach Morris", 9)
+school.roster
+
+school.grade(9)
+
+school.sort
